36fd7884c0054f76cf3604bc1eccf16be1f23911
Hao Xu <proverbsonly@gmail.com>
2019-09-29 12:45:06 -0700

    add customized apis
    add CRUD endpoints for Energy model

--- script/script/urls.py
+++ script/script/urls.py
@@ -1,18 +1,23 @@
+from django.urls import path, include
 from rest_framework import routers
-from .api import CountyViewSet, EnergyViewSet, ChargingStationViewSet
+from script.views import CountyViewSet, EnergyViewSet, EnergyView, ChargingStationViewSet
 
 # set up a router for RESTful API
 # ref1: https://www.django-rest-framework.org/api-guide/routers/
 # ref2: https://www.django-rest-framework.org/api-guide/filtering/
 # an idea: /api/<algorithm_name>/<unique_hash_of_parameter>, if algorithm parameters are not too many
+# router.register('api/energy/county/(?P<county>[-\w]+)/year/(?P<year>[-\w]+)/month/(?P<month>[-\w]+)', EnergyView.as_view(), 'energy')
 
 # Tip:
 # 1. replace space with %20 in the request urls
 
 router = routers.DefaultRouter()
-router.register('api/county', CountyViewSet, 'county')
-router.register('api/energy', EnergyViewSet, 'energy')
-router.register('api/charging_station', ChargingStationViewSet, 'charging_station')
+router.register('county', CountyViewSet, 'county')
+router.register('energy', EnergyViewSet, 'energy')
+router.register('charging_station', ChargingStationViewSet, 'charging_station')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('debug/', include(router.urls)),
+    path('api/energy', EnergyView.as_view()),
+]
 
--- script/script/views.py
+++ script/script/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
+from rest_framework import views, viewsets, permissions, mixins, generics
+from script.models import County, Energy, ChargingStation
+from .serializers import CountySerializer, EnergySerializer, ChargingStationSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+import json
+
+
+class CountyViewSet(viewsets.ModelViewSet):
+    queryset = County.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = CountySerializer
+    # currently POST is allowed just for debugging
+    # http_method_names = ['get',]
+
+
+class EnergyViewSet(viewsets.ModelViewSet):
+    queryset = Energy.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = EnergySerializer
+
+
+class ChargingStationViewSet(viewsets.ModelViewSet):
+    queryset = ChargingStation.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = ChargingStationSerializer
+
+
+class EnergyView(mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.CreateModelMixin,
+                        mixins.DestroyModelMixin,
+                        generics.GenericAPIView):
+    queryset = Energy.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = EnergySerializer
+
+    # [TODO] get & validate the params in an additional func
+
+    def get(self, request, *args, **kwargs):
+        county_name = request.query_params.get('county', None)
+        county = County.objects.get(name=county_name)
+        year = request.query_params.get('year', None)
+        month = request.query_params.get('month', None)
+        res = Energy.objects.get(county=county, year=year, month=month)
+        serializer = self.serializer_class(res)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        county_name = request.query_params.get('county', None)
+        county = County.objects.get(name=county_name)
+        year = request.query_params.get('year', None)
+        month = request.query_params.get('month', None)
+        energy = request.query_params.get('energy', None)
+        res = Energy.objects.get(county=county, year=year, month=month)
+        res.energy = energy
+        res.save()
+        serializer = self.serializer_class(res)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        county_name = request.query_params.get('county', None)
+        county = County.objects.get(name=county_name)
+        year = request.query_params.get('year', None)
+        month = request.query_params.get('month', None)
+        energy = request.query_params.get('energy', None)
+        res = Energy.objects.create(county=county, year=year, month=month, energy=energy)
+        serializer = self.serializer_class(res)
+        return Response(serializer.data)
+
+    def delete(self, request, *args, **kwargs):
+        county_name = request.query_params.get('county', None)
+        county = County.objects.get(name=county_name)
+        year = request.query_params.get('year', None)
+        month = request.query_params.get('month', None)
+        res = Energy.objects.get(county=county, year=year, month=month)
+        res.delete()
+        serializer = self.serializer_class(res)
+        return Response(serializer.data)
+
