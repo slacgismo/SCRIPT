0956a37f29d784a5c3d56ed81815572f7a66441f
Yanqing Wang <wangyanqingchn@gmail.com>
2019-09-29 17:01:18 -0700

    Style react vector map

--- frontend/src/components/OverviewMap.js
+++ frontend/src/components/OverviewMap.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 import VectorMap from '@south-paw/react-vector-maps';
-import caMapData from '@south-paw/react-vector-maps/maps/json/usa-ca.json';
+import caMapData from '@south-paw/react-vector-maps/maps/json/usa-ca.json'
+import { Wrapper, Output, MapWrapper } from './overviewMapStyled';
 
 class OverviewMap extends React.Component {
   render () {
+    const StyledMap = styled(MapWrapper)`
+      svg {
+        path {
+          fill: #3d0043;
+          cursor: pointer;
+
+          &:hover {
+            fill: #90007f;
+          }
+
+          &[aria-current='true'] {
+            fill: #d52484;
+          }
+        }
+      }
+    `;
+
     return (
-      <VectorMap { ...caMapData } />
+      <StyledMap>
+        <VectorMap
+          style={{ width: "30%" }} // TODO: remove this line
+          { ...caMapData }
+        />
+      </StyledMap>
     )
   }
 }
