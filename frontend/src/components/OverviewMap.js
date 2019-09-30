0c31813be19c351d107339dbbd8694d3c4b8e823
Yanqing Wang <wangyanqingchn@gmail.com>
2019-09-30 15:40:03 -0700

    Improvement: avoid rendering duplicate div at each re-rendering time

--- frontend/src/components/OverviewMap.js
+++ frontend/src/components/OverviewMap.js
@@ -5,6 +5,30 @@ import VectorMap from '@south-paw/react-vector-maps';
 import caMapData from '@south-paw/react-vector-maps/maps/json/usa-ca.json'
 import { Wrapper, Output, MapWrapper } from './overviewMapStyled';
 
+const StyledMap = styled(MapWrapper)`
+  svg {
+    path {
+      fill: #3d0043;
+      cursor: pointer;
+
+      &:hover {
+        fill: #90007f;
+      }
+
+      &[aria-current='true'] {
+        fill: #d52484;
+      }
+    }
+  }
+`;
+
+const Tooltip = styled.div`
+  position: absolute;
+  padding: 0.25rem;
+  background: white;
+  border: 0.2rem solid #ccc;
+`;
+
 class OverviewMap extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -24,8 +48,8 @@ class OverviewMap extends React.PureComponent {
   onMouseMove = e => {
     this.setState({
       isTooltipVisible: true,
-      tooltipY: e.clientY + 10,
-      tooltipX: e.clientX + 10,
+      tooltipY: e.pageY + 10,
+      tooltipX: e.pageX + 10,
     })
   }
 
@@ -35,35 +59,12 @@ class OverviewMap extends React.PureComponent {
 
   render () {
     const { current, isTooltipVisible, tooltipX, tooltipY } = this.state;
+    
     const layerProps = {
       onMouseOver: this.onMouseOver,
       onMouseMove: this.onMouseMove,
       onMouseOut: this.onMouseOut,
     };
-
-    const StyledMap = styled(MapWrapper)`
-      svg {
-        path {
-          fill: #3d0043;
-          cursor: pointer;
-
-          &:hover {
-            fill: #90007f;
-          }
-
-          &[aria-current='true'] {
-            fill: #d52484;
-          }
-        }
-      }
-    `;
-
-    const Tooltip = styled.div`
-      position: absolute;
-      padding: 0.25rem;
-      background: white;
-      border: 0.2rem solid #ccc;
-    `;
 
     const tooltipStyle = {
       display: isTooltipVisible ? 'block' : 'none',
@@ -72,14 +73,17 @@ class OverviewMap extends React.PureComponent {
     };
 
     return (
-      <StyledMap>
-        <VectorMap
-          style={{ width: "30%" }} // TODO: remove this line
-          { ...caMapData }
-          layerProps={ layerProps } 
-        />
-        <Tooltip style={tooltipStyle}>{ current }</Tooltip>
-      </StyledMap>
+      <Wrapper
+        style={{ width: "600px" }}
+      >
+        <StyledMap>
+          <VectorMap
+            { ...caMapData }
+            layerProps={ layerProps } 
+          />
+          <Tooltip style={tooltipStyle}>{ current }</Tooltip>
+        </StyledMap>
+      </Wrapper>
     )
   }
 }
