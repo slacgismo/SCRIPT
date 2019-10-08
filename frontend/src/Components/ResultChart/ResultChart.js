00aaed3735337e8fd440edaea87ca40e43a525b5
Yanqing Wang <wangyanqingchn@gmail.com>
2019-10-07 22:09:30 -0700

    Implement basic chart

--- frontend/src/Components/ResultChart/ResultChart.js
+++ frontend/src/Components/ResultChart/ResultChart.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import '../../../node_modules/react-vis/dist/style.css';
+import {XYPlot, LineSeries, HorizontalGridLines, XAxis, YAxis,} from 'react-vis';
 
 class ResultChart extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const data = [
+      {x: 0, y: 3},
+      {x: 1, y: 5},
+      {x: 2, y: 10},
+      {x: 3, y: 9},
+      {x: 4, y: 13},
+      {x: 5, y: 15},
+      {x: 6, y: 10},
+      {x: 7, y: 3},
+      {x: 8, y: 4},
+      {x: 9, y: 5}
+    ];
     return (
-      <div>chart...</div>
-    )
+      <div className="App">
+        <XYPlot height={300} width={300}>
+          {/* <HorizontalGridLines /> */}
+          <LineSeries data={data} />
+          <XAxis />
+          <YAxis />
+        </XYPlot>
+      </div>
+    );
   }
 }
 
