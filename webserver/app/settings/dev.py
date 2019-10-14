be02cd2d48a6fde51adb87e5feb750ed5c4f35f6
Hao Xu <proverbsonly@gmail.com>
2019-10-14 16:57:01 -0700

    update settings for deployment

--- webserver/app/settings/dev.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-# override the settings from base.py
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
