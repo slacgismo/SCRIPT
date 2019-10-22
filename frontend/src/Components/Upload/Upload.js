450997fc0bc253ceb9a64a91e81ba008d49045b3
xinyi le <xinyile@xinyideMBP.attlocal.net>
2019-10-22 00:30:30 -0700

    add upload file ui

--- frontend/src/Components/Upload/Upload.js
+++ frontend/src/Components/Upload/Upload.js
@@ -1,14 +1,43 @@
-import React, {Component} from 'react';
-import Base from '../../Layouts/Base'
+import React, {Component} from "react";
+import Base from "../../Layouts/Base";
+import {DropzoneArea} from "material-ui-dropzone";
 
 class Upload extends Component {
-  render() {
-    return (
-      <div >
-        <Base />
-      </div>
-    );
-  }
+  
+	constructor(props){
+		super(props);
+		this.state = {
+			files: []
+		};
+	}
+	handleChange(files){
+		this.setState({
+			files: files
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<Base 
+					content = {
+						<div>
+							<DropzoneArea 
+								acceptedFiles={["text/plain"]}
+								dropzoneText = "Drag and drop a file here or click"
+								showPreviews = {true}
+								showPreviewsInDropzone = {false}
+								filesLimit = "1"
+								maxFileSize={5000000}
+								showFileNamesInPreview = "true"
+								onChange={this.handleChange.bind(this)} 
+							/>          
+						</div>
+					}
+				/>
+			</div>
+		);
+	}
 }
 
 export default Upload;
