b7d4da55fb5872866ff74bc951db06781662175e
Hao Xu <proverbsonly@gmail.com>
2019-10-22 16:02:27 -0700

    modify the algorithm model of load_controller

--- webserver/script/models/algorithms.py
+++ webserver/script/models/algorithms.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 from script.models.data import County
+from script.validators import validate_positive
 
 # Create your models of algorithm results here.
 
 class LoadController(models.Model):
     """Algorithm: Load Controller
-        inputs: county
+        inputs:
+            (1) county
+            (2) rate_energy_peak
+            (3) rate_energy_partpeak
+            (4) rate_energy_offpeak
+            (5) rate_demand_peak
+            (6) rate_demand_partpeak
+            (7) rate_demand_overall
         outputs：
             (1) uncontrolled load
             (2) controlled load (cvx optimized)
@@ -16,6 +24,12 @@ class LoadController(models.Model):
     """
     
     county = models.ForeignKey(County, on_delete=models.CASCADE)
+    rate_energy_peak = models.FloatField(validators=[validate_positive])
+    rate_energy_partpeak = models.FloatField(validators=[validate_positive])
+    rate_energy_offpeak = models.FloatField(validators=[validate_positive])
+    rate_demand_peak = models.FloatField(validators=[validate_positive])
+    rate_demand_partpeak = models.FloatField(validators=[validate_positive])
+    rate_demand_overall = models.FloatField(validators=[validate_positive])
     uncontrolled_load = JSONField()
     controlled_load = JSONField()
 
