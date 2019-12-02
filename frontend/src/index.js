841ff8a9273e4a5e2fac5b75684cf5eebc7dc52a
Yanqing Wang <wangyanqingchn@gmail.com>
2019-12-01 19:02:10 -0800

    Refactor algorithm3 (CBA)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import Upload from "./Components/Upload/Upload";
 import Download from "./Components/Download/Download";
 import AlgorithmPageLoadControll from "./Components/AlgorithmPage/AlgorithmPageLoadControll";
 import AlgorithmPageLoadForecast from "./Components/AlgorithmPage/AlgorithmPageLoadForecast";
-import Algorithm3 from "./Components/AlgorithmPage/Algorithm3";
+import AlgorithmPageCBA from "./Components/AlgorithmPage/AlgorithmPageCBA";
 import About from "./Components/About/About";
 import * as serviceWorker from "./serviceWorker";
 
@@ -18,7 +18,7 @@ render((
         <Route exact path="/Upload" component={ Download } />
         <Route exact path="/Algorithm1" component={ AlgorithmPageLoadControll } />
         <Route exact path="/Algorithm2" component={ AlgorithmPageLoadForecast } />
-        <Route exact path="/Algorithm3" component={ Algorithm3 } />
+        <Route exact path="/Algorithm3" component={ AlgorithmPageCBA } />
         <Route exact path="/About" component={ About } />
   
         {/* Routes for debugging single components. */}
