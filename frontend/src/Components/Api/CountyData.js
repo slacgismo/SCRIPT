31e5bcda7000fb67fe10e9c6e4b8710277e72503
Yanqing Wang <wangyanqingchn@gmail.com>
2019-12-05 07:21:57 +0000

    Export after definition

--- frontend/src/Components/Api/CountyData.js
+++ frontend/src/Components/Api/CountyData.js
@@ -1,21 +1,12 @@
 import axios from "axios";
 import { serverUrl } from './server'
 
-// Fetch basic data
-//
-// axios('http://127.0.0.1:8000/api/county/')
-// .then(res => {
-//     const counties = res.data
-//     console.log(counties)
-//     this.setState({ counties })
-//     console.log(this.state.counties)
-// })
-// .catch(console.log)
-
-export let countyRes = [];
+let countyRes = [];
 
 function fetchCounty() {
     countyRes = axios.get(`${serverUrl}/county`);
 }
 
 fetchCounty();
+
+export { countyRes };
