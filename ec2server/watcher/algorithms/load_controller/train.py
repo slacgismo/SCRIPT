bbc4133a6d9b6ca3b2272a84d95b30e640f09392
Hao Xu <proverbsonly@gmail.com>
2019-12-18 10:11:17 -0800

    add args of model_name

--- ec2server/watcher/algorithms/load_controller/train.py
+++ ec2server/watcher/algorithms/load_controller/train.py
@@ -16,6 +16,7 @@ parser.add_argument('--rate_demand_partpeak', type=float, default=5.85)
 parser.add_argument('--rate_demand_overall', type=float, default=19.10)
 parser.add_argument('--s3_session_path', type=str, help='An s3 .csv file or a prefix', default='s3://script.chargepoint.data/clean/sessions_paired.csv')
 parser.add_argument('--s3_interval_path', type=str, help='An s3 .csv file or a prefix', default='s3://script.chargepoint.data/clean/intervals_paired.csv')
+parser.add_argument('--model_name', type=str, help='Prefix of model name', default='test')
 args = parser.parse_args()
 
 county_list = [county.replace('-', ' ') for county in args.county]
@@ -27,6 +28,7 @@ rate_demand_partpeak = args.rate_demand_partpeak
 rate_demand_overall = args.rate_demand_overall
 s3_session_path = args.s3_session_path
 s3_interval_path = args.s3_interval_path
+model_name = args.model_name
 
 # cache data(run when data collection changes)
 load_control_algorithm.LoadControlAlgorithm.cache_data(s3_session_path, s3_interval_path)
@@ -41,4 +43,4 @@ for county in county_list:
                                                         rate_demand_partpeak,
                                                         rate_demand_overall)
     lca.generate_profiles(50, 200, CHARGE_RATE)
-    lca.generate_model('test_model_{}'.format(county.replace(' ', '_')))
+    lca.generate_model('{}_{}'.format(model_name, county.replace(' ', '_')))
