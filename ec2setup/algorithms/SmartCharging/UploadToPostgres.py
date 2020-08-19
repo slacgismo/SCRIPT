4e3c4ceeb71d6864a27b1b8b953de7251d04d875
Anna Peery <avpeery@gmail.com>
2020-08-19 14:18:16 -0700

    WIP: editing queries correctly to establish foreign key relationships in smartcharging/uploadtopostgres.py

--- ec2setup/algorithms/SmartCharging/UploadToPostgres.py
+++ ec2setup/algorithms/SmartCharging/UploadToPostgres.py
@@ -91,32 +91,41 @@ class UploadToPostgres():
             " (name, total_energy, total_session, peak_energy)" + \
             " VALUES (%s, %s, %s, %s)",
             (
-                self.county, str(self.total_energy), str(self.total_number_of_session), str(self.rate_energy_peak)
-            )
-        )
-
-        cur.execute("INSERT INTO " + self.table_name + \
-            " (county, rate_energy_peak, rate_energy_partpeak, rate_energy_offpeak," + \
-            " rate_demand_peak, rate_demand_partpeak, rate_demand_overall, uncontrolled_load, controlled_load)" + \
-            " VALUES (%s, %s, %s, %s, %s, %s, %s)",
-            (
-                self.county, str(self.rate_energy_peak), str(self.rate_energy_partpeak), str(self.rate_energy_offpeak),
-                str(self.rate_demand_peak), str(self.rate_demand_partpeak), str(self.rate_demand_overall)
+                self.county, self.total_energy, self.total_number_of_session, self.rate_energy_peak
             )
         )
 
         conn.commit()
 
-        config_id = cur.execute("SELECT id FROM " + self.table_name + \
-            "ORDER BY id " + \
-            "DESC " + \
-            "LIMIT 1")
+        county = cur.execute("SELECT * FROM script_county " + \
+            "WHERE name = \"" + self.county +"\";")
+
+        cur.execute("INSERT INTO " + self.table_name + \
+            " (county, rate_energy_peak, rate_energy_partpeak, rate_energy_offpeak," + \
+            " rate_demand_peak, rate_demand_partpeak, rate_demand_overall)" + \
+            " VALUES (%s, %s, %s, %s, %s, %s, %s)",
+            (
+                county, self.rate_energy_peak, self.rate_energy_partpeak, self.rate_energy_offpeak,
+                self.rate_demand_peak, self.rate_demand_partpeak, self.rate_demand_overall
+            )
+        )
+
+        conn.commit()
+
+        config = cur.execute("SELECT * FROM " + self.table_name + \
+            " WHERE county = \"" + county + "\"" \
+            " AND rate_energy_peak = " + self.rate_energy_peak + \
+            " AND rate_energy_partpeak = " + self.rate_energy_partpeak + \
+            " AND rate_energy_offpeak = " + self.rate_energy_offpeak + \
+            " AND rate_demand_peak = " + self.rate_demand_peak + \
+            " AND rate_demand_partpeak = " + self.rate_demand_partpeak + \
+            " AND rate_demand_overall = " + self.rate_demand_overall + ";")
 
         cur.execute("INSERT INTO script_algorithm_load_controller" + \
             " (config, uncontrolled_load, controlled_load)" + \
             " VALUES (%s, %s, %s)",
             (
-                config_id, json.dumps(baseline_profiles_list), json.dumps(controlled_profiles_list) 
+                config, json.dumps(baseline_profiles_list), json.dumps(controlled_profiles_list) 
             )
         )
 
