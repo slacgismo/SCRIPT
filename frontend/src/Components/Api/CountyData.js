7a6604d668b3b29c4cfb9f482d46c356bae92e98
Anna Peery <avpeery@gmail.com>
2020-08-31 14:55:23 -0700

    Removed extra console.log statement

--- frontend/src/Components/Api/CountyData.js
+++ frontend/src/Components/Api/CountyData.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { serverUrl } from "./server";
 
-console.log('SERVER URL', serverUrl)
 let countyRes = [];
 countyRes = axios.get(`${serverUrl}/county`);
 export { countyRes };
