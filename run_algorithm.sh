9b37b0e017ad214a41e19c5548a3a6d4b108e903
Derin Serbetcioglu <serbetd@gmail.com>
2020-10-30 16:02:32 -0700

    new branch for clean-up/readme update

--- run_algorithm.sh
+++
@@ -1,4 +0,0 @@
-#cd ~/SCRIPT/ec2setup/algorithms/SmartCharging
-#python3 SmartChargingAlgorithm.py
-cd ~/SCRIPT/ec2setup/algorithms/CostBenefitAnalysis/cases/basecase/results/
-python3 UploadToPostgres.py
